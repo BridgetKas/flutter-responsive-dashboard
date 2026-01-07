a91f52b099cd659d66f3a6e39880c245f30ef90e
Bridget <bridgetnamugga@gmail.com>
2026-01-07 13:27:22 +0300

    Added responsive class

--- lib/responsive-dashboard/Config/responsive.dart
+++ lib/responsive-dashboard/Config/responsive.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Responsive extends StatelessWidget {
-  
-  const Responsive({super.key});
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget desktop;
+
+  const Responsive({super.key, required this.mobile, this.tablet, required this.desktop});
+
+  // this function checks whether the current screen is a mobile device
+  static bool isMobile(BuildContext context) => MediaQuery.of(context).size.width < 767;
+
+  static bool isTablet(BuildContext context) => MediaQuery.of(context).size.width < 1024 && MediaQuery.of(context).size.width >= 768;
+
+  static bool isDesktop(BuildContext context) => MediaQuery.of(context).size.width >= 1024;
+
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Size size = MediaQuery.of(context).size;
+
+    if(size.width >= 1025) {
+      return desktop;
+    }else if(size.width >= 768 && tablet != null){
+      return tablet!;
+    }else {
+      return mobile;
+    }
   }
 }
