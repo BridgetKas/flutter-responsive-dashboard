e8969e233b9aafb823b92cdbbd7c4184aecea9a5
Bridget <bridgetnamugga@gmail.com>
2026-01-08 09:58:57 +0300

    Added menu icons

---
+++ lib/Model/model.dart
@@ -0,0 +1,9 @@
+const menuIcons = [
+  'dashboard/assets/dashboard/home-1-svgrepo-com.svg',
+  'dashboard/assets/dashboard/invoice-svgrepo-com.svg',
+  'dashboard/assets/dashboard/trophy-svgrepo-com.svg',
+  'dashboard/assets/dashboard/pie-chart-2-svgrepo-com.svg',
+  'dashboard/assets/dashboard/credit-card-alt-svgrepo-com.svg',
+  'dashboard/assets/dashboard/clipboard-svgrepo-com.svg'
+
+];
