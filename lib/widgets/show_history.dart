c89bb387bd821e728e14c80dd9d7b47fb4eddeaa
Bridget <bridgetnamugga@gmail.com>
2026-01-09 15:49:43 +0300

    Added show history widget

---
+++ lib/widgets/show_history.dart
@@ -0,0 +1,86 @@
+import 'package:dashboard/Model/model.dart';
+import 'package:dashboard/responsive-dashboard/Config/responsive.dart';
+import 'package:dashboard/responsive-dashboard/Config/size_config.dart';
+import 'package:dashboard/utilis/colors.dart';
+import 'package:flutter/material.dart';
+
+class ShowHistory extends StatefulWidget {
+  const ShowHistory({super.key});
+
+  @override
+  State<ShowHistory> createState() => _ShowHistoryState();
+}
+
+class _ShowHistoryState extends State<ShowHistory> {
+  int selectedIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return SingleChildScrollView(
+      scrollDirection: Responsive.isDesktop(context) ? Axis.vertical : Axis.horizontal,
+      child: SizedBox(
+        width: Responsive.isDesktop(context) 
+        ? double.infinity 
+        : SizeConfig.screenWidth,
+        child: Table(
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: List.generate(transactionHistory.length, 
+          (index) {
+            final transaction = transactionHistory[index];
+            return TableRow(
+              decoration: BoxDecoration(
+                color: selectedIndex == index 
+                  ? Colors.white 
+                  : MyAppColor.backgroundColor,
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(
+                    top: 10,
+                    bottom: 10,
+                    left: 20
+                  ),
+                  child: CircleAvatar(
+                    radius: 17,
+                    backgroundImage: AssetImage(transaction.avatar),
+
+                  ),
+                ),
+                Text(
+                  transaction.label,
+                  style: TextStyle(
+                    letterSpacing: -1,
+                    height: 1.3,
+                    color: MyAppColor.secondary
+                  ),),
+                  Text(
+                    transaction.time,
+                    style: TextStyle(
+                      letterSpacing: -1,
+                      height: 1.3,
+                      color: MyAppColor.secondary
+                  ),),
+                   Text(
+                   "${transaction.amount}.00" ,
+                    style: TextStyle(
+                      letterSpacing: -1,
+                      height: 1.3,
+                      color: MyAppColor.secondary
+                  ),),
+                   Text(
+                   transaction.status ,
+                    style: TextStyle(
+                      letterSpacing: -1,
+                      height: 1.3,
+                      color: MyAppColor.secondary
+                  ),),
+              ]
+            );
+          },
+        ),
+      ),
+    ));
+  }
+}
