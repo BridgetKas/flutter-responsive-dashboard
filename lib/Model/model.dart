2e6c6cc3a73a9e2905e9e833f706e999688e0e89
Bridget <bridgetnamugga@gmail.com>
2026-01-09 09:09:56 +0300

    Added infocard model

--- lib/Model/model.dart
+++ lib/Model/model.dart
@@ -1,9 +1,43 @@
 const menuIcons = [
-  'dashboard/assets/dashboard/home-1-svgrepo-com.svg',
-  'dashboard/assets/dashboard/invoice-svgrepo-com.svg',
-  'dashboard/assets/dashboard/trophy-svgrepo-com.svg',
-  'dashboard/assets/dashboard/pie-chart-2-svgrepo-com.svg',
-  'dashboard/assets/dashboard/credit-card-alt-svgrepo-com.svg',
-  'dashboard/assets/dashboard/clipboard-svgrepo-com.svg'
+  'assets/dashboard/home-1-svgrepo-com.svg',
+  'assets/dashboard/invoice-svgrepo-com.svg',
+  'assets/dashboard/trophy-svgrepo-com.svg',
+  'assets/dashboard/pie-chart-2-svgrepo-com.svg',
+  'assets/dashboard/credit-card-alt-svgrepo-com.svg',
+  'assets/dashboard/clipboard-svgrepo-com.svg'
 
 ];
+
+
+class InfoCardModel {
+   final String icon;
+   final String label;
+   final String amount;
+
+   InfoCardModel({required this.icon, required this.label, required this.amount});
+}
+
+final List<InfoCardModel> infoCardData = [
+  InfoCardModel(
+    icon: 'assets/dashboard/credit-card-alt-svgrepo-com.svg', 
+    label: 'Transfer via \nCard number', 
+    amount: '\$1200'
+  ),
+    InfoCardModel(
+    icon: 'assets/dashboard/transfer-horizontal-svgrepo-com.svg', 
+    // icon: 'assets/dashboard/credit-card-alt-svgrepo-com.svg', 
+    label: 'Transfer via \nOnline Banks', 
+    amount: '\$150'
+  ),
+     InfoCardModel(
+    icon: 'assets/dashboard/bank-svgrepo-com.svg', 
+    label: 'Transfer via \nSame Bank', 
+    amount: '\$150'
+  ),
+     InfoCardModel(
+    icon: 'assets/dashboard/bank-svgrepo-com (1).svg', 
+    label: 'Transfer via \nOther Bank', 
+    amount: '\$150'
+  ),
+];
+
