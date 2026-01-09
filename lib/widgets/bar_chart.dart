aecf1b9e1a5ce141e9bd2f96032409aa5352ea02
Bridget <bridgetnamugga@gmail.com>
2026-01-09 10:29:19 +0300

    Added a bar chart

--- lib/widgets/bar_chart.dart
+++ lib/widgets/bar_chart.dart
@@ -1,3 +1,5 @@
+import 'package:dashboard/responsive-dashboard/Config/responsive.dart';
+import 'package:dashboard/utilis/colors.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
@@ -6,23 +8,118 @@ class MyBarChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: AspectRatio(
-        aspectRatio: 1.6, // Ensures the chart doesn't try to expand infinitely
-        child: BarChart(
-          BarChartData(
-            borderData: FlBorderData(show: false),
-            alignment: BarChartAlignment.spaceBetween,
-            gridData: const FlGridData(
-              show: false),
-            maxY: 20, // Define the max Y value for scaling
-
-          
-          ),
+    return BarChart(
+      BarChartData(
+        barGroups: _chartValue(context),
+        borderData: FlBorderData(show: false),
+        alignment: BarChartAlignment.spaceAround,
+        gridData: FlGridData(
+          drawHorizontalLine: true,
+          horizontalInterval: 30,
         ),
+        titlesData: FlTitlesData(
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              reservedSize: 40,
+              interval: 30,
+              getTitlesWidget: (value,meta) {
+                switch(value.toInt()) {
+                  case 0:
+                  return const Text('0',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                  ),);
+                  case 10:
+                  return const Text('0',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                  ),);
+                  case 50:
+                  return const Text('50k',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                  ),);
+                  case 100:
+                  return const Text('100k',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                  ),);
+                  default: return SizedBox.shrink();
+                }
+              }
+            )
+          ),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              reservedSize: 40,
+              getTitlesWidget: (value, meta) {
+                final months = [
+                  'JAN',
+                  'FEB',
+                  'MAR',
+                  'APR',
+                  'MAY',
+                  'JUN',
+                  'JUL',
+                  'AUG',
+                  'SEP',
+                  'OCT',
+                  'NOV',
+                  'DEC'
+                ];
+                if(value.toInt() >= 0 && value.toInt() < months.length){
+                  return Text(
+                    months[value.toInt()], 
+                    style: TextStyle(fontSize: 12,color: Colors.grey),
+                  );
+                }
+
+                return const SizedBox.shrink();
+              },
+            )
+          )
+
+        )
       ),
+      duration: const Duration(milliseconds: 150),
+      curve: Curves.linear,
     );
+  }
+
+  List<BarChartGroupData> _chartValue(BuildContext context) {
+    final List<double> values = [
+      20,
+      35,
+      50,
+      90,
+      60,
+      40,
+      25,
+      15,
+      70,
+      90,
+      70,
+      60
+    ];
+    return List.generate(values.length, (index) {
+      return BarChartGroupData(x: index, barsSpace: 4,barRods: [
+        BarChartRodData(toY: values[index],
+        color: Colors.black,
+        width: Responsive.isDesktop(context) ? 40: 10,
+        borderRadius: BorderRadius.zero,
+        backDrawRodData: BackgroundBarChartRodData(
+          show: true,
+          toY: 90,
+          color: MyAppColor.barBg
+        ))
+      ]);
+    });
   }
 
 }
